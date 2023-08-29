0cca156eaa2547ecbdfbad29c0f2c062fbb7a651
guillaume planche <gplanche45@gmail.com>
2023-08-29 20:54:26 +0200

    db modif

--- app/controllers/stories_controller.rb
+++ app/controllers/stories_controller.rb
@@ -1,33 +1,37 @@
 class StoriesController < ApplicationController
+  before_action :set_kid, only: [:new, :create, :show]
 
   def new
     @story = Story.new
   end
 
   def create
-    @story = Story.new(story_params)
+    @story = @kid.stories.build(story_params)
     @story.content = generate_story_content(@story)
+
     if @story.save
-      redirect_to kid_story_path(@story.kid, @story)
+      redirect_to kid_story_path(@kid, @story)
     else
       render :new
     end
   end
 
+  def show
+    @story = Story.find(params[:id])
+  end
+
   private
+
+  def set_kid
+    @kid = Kid.find(params[:kid_id])
+  end
 
   def story_params
     params.require(:story).permit(:story_type, :environment, :duration)
   end
 
   def generate_story_content(story)
-    # Use the OpenAI gem to generate the story content based on the user's preferences
-    response = Openai::Completion.create(
-      model: "gpt-3.5-turbo",
-      prompt: "Create a #{story.duration} #{story.theme} story set in a #{story.environment}.",
-      max_tokens: 500 # Adjust as needed
-    )
-    response.choices.first.text.strip
+    prompt = "Create a #{story.duration} #{story.story_type} story set in a #{story.environment}."
+    ChatGptService.generate_story(prompt)
   end
-
 end
