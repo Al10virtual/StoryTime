3a26b3392abd0b441f28972cb4f868c20da02b32
Alexis Landois <alexis.landois17@gmail.com>
2023-08-30 17:47:12 +0200

    story new

--- app/models/story.rb
+++ app/models/story.rb
@@ -1,4 +1,10 @@
 class Story < ApplicationRecord
+  THEMES = ["Aventure", "Conte de fée", "Fantaisie", "Historique", "Mystère", "Educatif"]
   belongs_to :kid
   has_one_attached :audio
+  enum :lenght, {
+    Courte: 0,
+    Moyenne: 1,
+    Longue: 2
+  }
 end
