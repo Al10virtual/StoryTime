f81ade4e72bf62c63d0fc0a73de9f5b669d98481
Arnaud Legaignoux <arnaud.legaignoux@gmail.com>
2023-08-31 16:23:40 +0200

    sync

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_08_30_151144) do
+ActiveRecord::Schema[7.0].define(version: 2023_08_31_140657) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -66,6 +66,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_08_30_151144) do
     t.bigint "family_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "avatar_file_name"
     t.index ["family_id"], name: "index_kids_on_family_id"
   end
 
