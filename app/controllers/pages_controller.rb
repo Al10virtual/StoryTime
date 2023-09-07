814aa7c3c8406c2abb8717656379e427e32344ba
guillaume planche <gplanche45@gmail.com>
2023-09-07 16:31:25 +0200

    home styling

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,5 +2,6 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [ :home ]
 
   def home
+    @family = current_user.families.first if user_signed_in?
   end
 end
