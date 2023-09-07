c75bf3b125bb1f1f193d6b209d464bf084918864
Arnaud Legaignoux <arnaud.legaignoux@gmail.com>
2023-09-07 22:20:47 +0200

    ok

--- db/migrate/20230829115334_add_prompt_and_lenght_and_duration_to_stories.rb
+++ db/migrate/20230829115334_add_prompt_and_lenght_and_duration_to_stories.rb
@@ -1,7 +1,7 @@
 class AddPromptAndlengthAndDurationToStories < ActiveRecord::Migration[7.0]
   def change
     add_column :stories, :prompt, :text
-    add_column :stories, :length, :integer
+    add_column :stories, :lenght, :integer
     add_column :stories, :duration, :integer
   end
 end
