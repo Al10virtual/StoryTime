765ee4b524f476399d9cc4bf96fbdc44f608714d
Arnaud Legaignoux <arnaud.legaignoux@gmail.com>
2023-09-07 15:29:11 +0200

    it seems done

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
-  # def after_sign_in_path_for(resource)
-  #   stored_location_for(resource) || family_path
-  # end
+  def after_sign_in_path_for(resource)
+    stored_location_for(resource) || family_path(resource.families.first)
+  end
 end
