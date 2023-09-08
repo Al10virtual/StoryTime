6a724e07a94f9ba48470e5ae5387538c27a6eee2
Arnaud Legaignoux <arnaud.legaignoux@gmail.com>
2023-09-08 13:19:07 +0200

    save temporary files in public

--- app/jobs/generate_story_audio_job.rb
+++ app/jobs/generate_story_audio_job.rb
@@ -5,7 +5,7 @@ class GenerateStoryAudioJob < ApplicationJob
   def perform(story)
     base64mp3 = GoogleSpeechClient.new.get_sound(story.content)
     file_name = I18n.transliterate(story.title).gsub(/[^0-9A-Za-z]/, '_').downcase
-    save_path = Rails.root.join('public/speech', "#{file_name}.mp3")
+    save_path = Rails.root.join('public', "#{file_name}.mp3")
     File.open(save_path, "wb") do |file|
       file.write(Base64.decode64(base64mp3))
     end
